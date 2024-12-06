250d35bf1699d170895bb433a4eb7a0cb033dd11
sanjay gupta <guptaji30749@gmail.com>
2024-12-06 17:54:41 +0530

    mother

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 
 import React from 'react'
 import Admin from './Component/admin/Admin'
-import FrontendProject from "./Component/FrontendProject"
+
 import { BrowserRouter, Route , Routes } from 'react-router-dom'
-import  Desh from "./Component/Deshboard/Desh"
+
 import Counter from "./Component/Counter/Counterr"
 // import "./App.css"
 const App = () => {
@@ -12,8 +12,8 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path='' element={<Admin/>}/>
-          <Route path='/frontendProject' element={<FrontendProject/>}/>
-          <Route path='/desh' element={<Desh/>}/>
+          
+         
           <Route path='/counterr' element={<Counter/>}/>
         </Routes>
       </BrowserRouter>
